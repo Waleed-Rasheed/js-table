b5d4bc01c0c5235813ceafcffccd5f97084def60
Waleed-Rasheed <usamapagal75@gmail.com>
2024-02-22 07:29:27 +0500

    Work Done

---
+++ app.js
@@ -0,0 +1,37 @@
+var num1 = +prompt("Enter your 1st value")
+var num2 = +prompt("Enter your 2nd value")
+
+var Addition = num1 + num2;
+var Subtract = num1 - num2;
+var Multiply = num1 * num2;
+var Divide = num1 / num2;
+var Divide = num1 % num2;
+
+document.write(
+
+    "<table border='1'>"+
+    "<tr>"+
+    "<th>"+"Addition"+"</th>"
+    +"<th>"+"Subtract"+"</th>"
+    +"<th>"+"Multiply"+"</th>"
+    +"<th>"+"Divide"+"</th>"
+    +"</tr>"+
+
+    "<tr>"+
+    "<td>"+Addition+"</td>"
+    +"<td>"+Subtract+"</td>"
+    +"<td>"+Multiply+"</td>"
+    +"<td>"+Divide+"</td>"
+    +"</tr>"
+
+
+    +"</table>"
+
+
+
+
+
+
+
+
+)
